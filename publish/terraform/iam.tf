a4e2625033c460ed0255a8b1803e574ded8cff2e
ddefreitas.exe <1959720+darrell-d@users.noreply.github.com>
2025-01-27 00:32:35 -0500

    update data arn

--- publish/terraform/iam.tf
+++ publish/terraform/iam.tf
@@ -122,8 +122,9 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn}/*",
-      data.terraform_remote_state.af_south_1.outputs.af_south_s3_bucket_arn,
-      "${data.terraform_remote_state.af_south_1.outputs.af_south_s3_bucket_arn}/*",
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn,
+      "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn}/*",
+
     ]
   }
 
